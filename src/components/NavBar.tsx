49ea5ad22cb52694b3a4b96401071394dd657fdc
August Dominik Raaschou-Pedersen <guciodevs@gmail.com>
2023-02-22 10:01:11 +0100

    Add animated hamburger.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
+import { Squash as Hamburger } from 'hamburger-react';
 import { HiOutlineMenu, HiX } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 
 export default function NavBar({showNavFullscreen, toggleNavFullscreen}:{showNavFullscreen:boolean, toggleNavFullscreen: () => void}) {
     return (
         <div className={"flex flex-col w-full justify-items-center pl-10 pr-10 text-2xl transition-colors z-50 sticky top-0 " 
-            + (showNavFullscreen ? 'text-white bg-none' : 'text-black bg-white')}>
+            + (showNavFullscreen ? 'text-black bg-none' : 'text-black bg-white')}>
             <div className="flex justify-between sm:grid sm:grid-cols-2 mt-6 mb-2">
-                <div className="flex justify-start gap-12">
+                <div className="flex justify-start gap-12 items-center">
                     <a className="nav-element">August Dominik</a>
                 </div>
                 <div className="flex justify-end gap-8">
@@ -14,13 +15,11 @@ export default function NavBar({showNavFullscreen, toggleNavFullscreen}:{showNav
                     <Link className="nav-element hidden sm:block" to={'/teknologier'}>Teknologier</Link>
                     <div className='flex items-center justify-center cursor-pointer sm:hidden'
                         onClick={() => toggleNavFullscreen()}>
-                        {showNavFullscreen 
-                            ? <HiX/>
-                            : <HiOutlineMenu/>}
+                        <Hamburger toggled={showNavFullscreen}/>
                     </div>
                 </div>
             </div>
-            <hr className={"border-t-2 transition-colors " + (showNavFullscreen ? 'border-white' : 'border-black')} />
+            <hr className={"border-t-2 transition-colors " + (showNavFullscreen ? 'border-black' : 'border-black')} />
         </div>
     )
 }
--- src/components/NavFullscreen.tsx
+++ src/components/NavFullscreen.tsx
@@ -2,10 +2,10 @@
 export default function NavFullscreen({ showNavFullscreen, setShowNavFullscreen }: { showNavFullscreen: boolean, setShowNavFullscreen: Function }) {
 
     return (
-        <div className={'w-full fixed z-50 left-0 top-0 bg-black overflow-hidden transition-height '
+        <div className={'w-full fixed z-50 left-0 top-0 bg-white overflow-hidden transition-height '
             + (showNavFullscreen ? 'h-full' : 'h-0')
             + ' pl-10'}>
-            <ul className={"text-white mt-24 [&>*]:mb-4 [&>*]:text-xl " 
+            <ul className={"text-black mt-24 [&>*]:mb-4 [&>*]:text-xl transition-all " 
                 + (showNavFullscreen ? 'opacity-100' : 'opacity-0')}>
                 <li className="nav-full-element">Hjem</li>
                 <li className="nav-full-element">Projekter</li>
